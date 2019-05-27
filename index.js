33d6527b92002c97547cb17c937fe86741010d58
Robyn Moscowitz <robyn.moscowitz@gmail.com>
2019-05-27 16:11:47 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,93 @@
+(async () => {
+  const fs = require('fs-extra');
+  const puppeteer = require('puppeteer');
+  const GIFEncoder = require('gif-encoder');
+  const getPixels = require('get-pixels');
+
+  const encoder = new GIFEncoder(800, 600);
+  const workDir = './temp';
+  const outDir = './out';
+
+  let firstImage = undefined;
+  let lastImage = undefined;
+
+  async function prepDirectory() {
+    console.log(`new working directory ${workDir}`);
+    await fs.emptyDir(workDir);
+    console.log(`check output directory ${outDir}`);
+    await fs.ensureDir(outDir);
+  }
+
+  async function takeScreenshots() {
+    console.log('start puppeteer');
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto('https://www.google.com/maps/@29.7696912,-95.3576299,11.19z/data=!5m1!1e1');
+    await page.waitForSelector('#omnibox-container'); // search box
+    await page.evaluate(selector => {
+      const elements = document.querySelectorAll(selector);
+      elements.forEach(e => {
+        e.parentNode.removeChild(e);
+      });
+    }, '#omnibox-container');
+
+    for (let i = 0; i < 10; i++) {
+      const now = new Date();
+      const date = now.toLocaleDateString('en-US');
+      const time = now.toLocaleTimeString('en-US');
+      const timestamp = now.getTime();
+
+      if (i === 0) firstImage = timestamp;
+      else if (i === 9) lastImage = timestamp;
+
+      console.log(`taking screenshot ${i + 1}/10 -- ${date} ${time} (${timestamp})`);
+      await page.screenshot({path: `${workDir}/${timestamp}.png`});
+      await page.waitFor(1000);
+    }
+
+    console.log('close puppeteer');
+    await browser.close();
+  }
+
+  function addToGif(images, counter = 0) {
+    console.log(`adding image ${counter + 1}/10 to gif`);
+    getPixels(images[counter], async (err, pixels) => {
+      encoder.addFrame(pixels.data);
+      encoder.read();
+      if (counter === images.length - 1) {
+        encoder.finish();
+        await fs.remove(workDir);
+        console.log(`${workDir} removed`);
+        process.exit(0);
+      } else {
+        addToGif(images, ++counter);
+      }
+    });
+  }
+
+  function createGif() {
+    const listOfPNGs = fs.readdirSync(workDir)
+      .map(a => a.substr(0, a.length - 4)) // filename without '.png'
+      .sort((a, b) => a - b)
+      .map(a => `${workDir}/${a.substr(0, a.length)}.png`);
+
+    const outFile = `${firstImage}-to-${lastImage}`;
+    console.log(`create gif ${outDir}/${outFile}.gif`);
+    const stream = fs.createWriteStream(`${outDir}/${outFile}.gif`);
+
+    // configure encoder
+    encoder.setFrameRate(60);
+    encoder.pipe(stream);
+    encoder.setQuality(10);
+    encoder.setDelay(500);
+    encoder.writeHeader();
+    encoder.setRepeat(0);
+
+    addToGif(listOfPNGs);
+  }
+
+  await prepDirectory();
+  await takeScreenshots();
+  await createGif();
+})();
